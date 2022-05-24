a7ac182d1d42850bf8dd5d0bfd906ee485d94aea
DanaSurmei <97785175+DanaSurmei@users.noreply.github.com>
2022-05-24 22:06:59 +0300

    js M02 M03

---
+++ M01S03/ex05/app.js
@@ -0,0 +1,24 @@
+var person = {
+  name: 'Daniela',
+  surname: 'Surmei',
+  age: 33,
+  petOwner: false,
+  skills: ['html', 'javaScript', 'css'],
+  friends: [
+    {
+      name: 'Larry',
+      surname: 'Larryson',
+      age: 30,
+    },
+    {
+      name: 'Steven',
+      surname: 'Stevenson',
+      age: 31,
+    },
+    {
+      name: 'Carol',
+      surname: 'Carlson',
+      age: 29,
+    },
+  ],
+};
