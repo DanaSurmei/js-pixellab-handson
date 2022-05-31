c8921763f81baff1be2873e714b08eb4365f072a
DanaSurmei <97785175+DanaSurmei@users.noreply.github.com>
2022-05-31 22:01:36 +0300

    S04 ex 2 3 4 5

---
+++ M01S04/ex02/app.js
@@ -0,0 +1,17 @@
+var errorcode = 200;
+
+if (errorcode === 0) {
+  console.log('No error');
+} else if (errorcode === 100) {
+  console.log('Error 100 occured');
+} else if (errorcode === 200) {
+  console.log('Error 200 occured');
+} else if (errorcode === 300) {
+  console.log('Error 300 occured');
+} else if (errorcode === 400) {
+  console.log('Error 400 occured');
+} else if (errorcode === 500) {
+  console.log('Error 500 occured');
+} else {
+  console.log('Something else');
+}
